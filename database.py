b27d236ebcef7df789434a26248472cd8d387d35
Carlos Valdes <valdescarlos@gmail.com>
2022-10-26 19:32:00 -0300

    Database (Mongo)

---
+++ database.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class Database:
+    def __init__(self):
+        self.client = MongoClient(os.getenv("LOCAL_MONGODB"))
+        self.db = None
+    
+    def initialize(self, ):
+        self.db = self.client['fullstack']
+    
+    
+    def insert(self, collection, data):
+        self.db[collection].insert(data)
+
+    
+    def find(self, collection, query):
+        return self.db[collection].find(query)
+    
+    
+    def find_one(self, collection, query):
+        return self.db[collection].find_one(query)
+    
+    
+    def update(self, collection, query, data):
+        self.db[collection].update(query, data, upsert=True)
+    
+    
+    def remove(self, collection, query):
+        self.db[collection].remove(query)
