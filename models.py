84dbf125634055dd5fedc48842dd5192d3df996a
Carlos Valdes <valdescarlos@gmail.com>
2022-10-30 14:16:55 -0300

    Constructor Model

--- models.py
+++ models.py
@@ -4,3 +4,9 @@ class Signal:
     date: str = None
     close: float = None
     test: bool = False
+
+    def __init__(self, _side, _date, _close, _test):
+        self.side = _side
+        self.date = _date
+        self.close = _close
+        self.test = _test
