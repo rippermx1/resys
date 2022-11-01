b19d4773089547be58cd46ddc678db80f040a4a1
carlos <valdescarlos17@gmail.com>
2022-10-31 23:04:23 -0300

    Logger refactor to class

--- logger.py
+++ logger.py
@@ -2,7 +2,7 @@ import logging
 
 class Logger():
     
-    def __init__():
+    def __init__(self):
         logging.basicConfig(filename="./std.log", format='%(asctime)s %(message)s', filemode='w')
-        logger=logging.getLogger()
-        logger.setLevel(logging.DEBUG)
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
