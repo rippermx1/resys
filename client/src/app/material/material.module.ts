43ed426724c5dbf18e4c23a2a83b8c9e5532a092
Carlos Valdes <valdescarlos@gmail.com>
2022-11-25 21:09:37 -0300

    Navbar

--- client/src/app/material/material.module.ts
+++ client/src/app/material/material.module.ts
@@ -5,6 +5,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [],
@@ -13,14 +14,16 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatIconModule,
     MatExpansionModule,
     MatDividerModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatButtonModule
   ],
   exports: [
     MatCardModule,
     MatIconModule,
     MatExpansionModule,
     MatDividerModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
